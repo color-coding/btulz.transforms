41deca8917a0020c457a5229fee8e45e4982c963
牛加人等于朱 <niuren.zhu@icloud.com>
2018-03-26 13:40:25 +0800

    修改工程文件

--- btulz.transforms.core/src/main/java/org/colorcoding/tools/btulz/transformer/RuntimeParameter.java
+++ btulz.transforms.core/src/main/java/org/colorcoding/tools/btulz/transformer/RuntimeParameter.java
@@ -1,5 +1,6 @@
 package org.colorcoding.tools.btulz.transformer;
 
+import java.util.Date;
 import java.util.Random;
 import java.util.UUID;
 
@@ -7,6 +8,20 @@ public class RuntimeParameter {
 	public long newLong() {
 		Random rd = new Random();
 		return rd.nextLong();
+	}
+
+	public long newInt() {
+		Random rd = new Random();
+		return rd.nextInt();
+	}
+
+	public long newShort() {
+		Random rd = new Random();
+		return rd.nextInt(32767);
+	}
+
+	public long getTime() {
+		return (new Date()).getTime();
 	}
 
 	public String newUUID() {
